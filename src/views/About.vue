3dc241adb1432840d021eea255d9fbbe13c38e7a
車文超 <chewenchao@kurumabunchounoMacBook-Pro.local>
2021-07-07 23:20:36 +0900

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <v-snackbar v-model="snackTest" timeout="3000" centered>This is a test.</v-snackbar>
+    <v-btn @click="test">Test</v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    snackTest: false
+  }),
+  methods: {
+    test() {
+      this.snackTest = true
+    }
+  }
+}
+</script>
